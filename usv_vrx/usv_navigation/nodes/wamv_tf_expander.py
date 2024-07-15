b4cdb47e7095fbdbc13a690400fe6ea8dd47572e
Kevin Li <likevin9911@gmail.com>
2024-07-15 11:12:55 -0700

    ekflocalization is back!

--- usv_vrx/usv_navigation/nodes/wamv_tf_expander.py
+++ usv_vrx/usv_navigation/nodes/wamv_tf_expander.py
@@ -11,7 +11,7 @@ def handle_odom_pose(ros_odom_msg):
     br = tf2_ros.TransformBroadcaster()
 
     map_2_odom                 = geometry_msgs.msg.TransformStamped() # comment for gmapping
-    #odom_2_wamv_odom           = geometry_msgs.msg.TransformStamped()
+    #odom_2_base_link           = geometry_msgs.msg.TransformStamped()
     #wamv_base_link_2_base_link = geometry_msgs.msg.TransformStamped()
     
     map_2_odom.header.stamp = rospy.Time.now() # comment for gmapping
@@ -19,10 +19,10 @@ def handle_odom_pose(ros_odom_msg):
     map_2_odom.child_frame_id = "/odom" # comment for gmapping
     map_2_odom.transform.rotation.w = 1 # comment for gmapping
 
-    #odom_2_wamv_odom.header.stamp = rospy.Time.now()
-    #odom_2_wamv_odom.header.frame_id = "/odom"
-    #odom_2_wamv_odom.child_frame_id = "/wamv/odom"
-    #odom_2_wamv_odom.transform.rotation.w = 1
+    #odom_2_base_link.header.stamp = rospy.Time.now()
+    #odom_2_base_link.header.frame_id = "/odom"
+    #odom_2_base_link.child_frame_id = "/base_link"
+    #odom_2_base_link.transform.rotation.w = 1
 
     #wamv_base_link_2_base_link.header.stamp = rospy.Time.now()
     #wamv_base_link_2_base_link.header.frame_id = "/wamv/base_link"
@@ -30,7 +30,7 @@ def handle_odom_pose(ros_odom_msg):
     #wamv_base_link_2_base_link.transform.rotation.w = 1
 
     br.sendTransform(map_2_odom) # comment for gmapping
-    #br.sendTransform(odom_2_wamv_odom)
+    #br.sendTransform(odom_2_base_link)
     #br.sendTransform(wamv_base_link_2_base_link)
     
 def callback(data):
